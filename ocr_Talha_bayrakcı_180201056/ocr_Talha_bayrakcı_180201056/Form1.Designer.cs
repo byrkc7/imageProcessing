00b06e8d649ea14912a906a8c4775b5cc8bd5da2
talha <byrkc7@icloud.com>
2021-11-19 01:54:31 +0300

    ocrImageProcessing

---
+++ ocr_Talha_bayrakcı_180201056/ocr_Talha_bayrakcı_180201056/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ocr_Talha_bayrakcı_180201056
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(111, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 211);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(601, 69);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(270, 211);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Learn";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(695, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Compare";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(695, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1130, 296);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1130, 336);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Algerian", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(540, 389);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(292, 30);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "TALHA BAYRAKCI 180201056";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1012, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "V.Differences :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1012, 339);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "H.Differences :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ocr_Talha_bayrakcı_180201056/ocr_Talha_bayrakcı_180201056/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ocr_Talha_bayrakcı_180201056
+{
+    public partial class Form1 : Form
+    {
+        Bitmap IMG_Learn;
+        Bitmap IMG_Compare;
+
+        int[] Letter_F_V_Proj;
+        int[] Letter_F_H_Proj;
+
+        int[] Letter_V_Proj;//RECOGNATİON
+        int[] Letter_H_Proj;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string F_Name;
+            openFileDialog1.ShowDialog();
+            F_Name = openFileDialog1.FileName;
+            IMG_Learn = new Bitmap(F_Name);
+            pictureBox1.Image = IMG_Learn;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Letter_F_H_Proj = new int[IMG_Learn.Height];
+            Letter_F_V_Proj = new int[IMG_Learn.Width];
+
+            for (int y = 0; y < IMG_Learn.Height; y++)
+                Letter_F_H_Proj[y] = 0;//buraya bak
+
+            for (int x = 0; x < IMG_Learn.Width; x++)
+                Letter_F_V_Proj[x] = 0;
+
+            for (int y = 0; y < IMG_Learn.Height; y++)
+                for (int x = 0; x < IMG_Learn.Width; x++)
+                    if (IMG_Learn.GetPixel(x, y).R < 10)
+                    {
+                        Letter_F_H_Proj[y]++;
+                    }
+
+
+            for (int x = 0; x < IMG_Learn.Width; x++)
+                for (int y = 0; y < IMG_Learn.Height; y++)
+                    if (IMG_Learn.GetPixel(x, y).R < 10)
+                    {
+                        Letter_F_V_Proj[x]++;
+                    }
+            //Shiftting H
+            int Pivot;
+            for (Pivot = 0; Pivot < IMG_Learn.Height; Pivot++)
+                if (Letter_F_H_Proj[Pivot] > 0) break;
+
+
+            for (int y = 0; y < IMG_Learn.Height - Pivot; y++)
+            {
+                Letter_F_H_Proj[y] = Letter_F_H_Proj[y + Pivot];
+                Letter_F_H_Proj[y + Pivot] = 0;
+            }
+
+
+
+
+            //Shiftting Vertical
+            for (Pivot = 0; Pivot < IMG_Learn.Width; Pivot++)
+                if (Letter_F_V_Proj[Pivot] > 0) break;
+
+
+            for (int x = 0; x < IMG_Learn.Width - Pivot; x++)
+            {
+                Letter_F_V_Proj[x] = Letter_F_V_Proj[x + Pivot];
+                Letter_F_V_Proj[x + Pivot] = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < IMG_Compare.Width; x++)
+                for (int y = 0; y < IMG_Compare.Height; y++)
+                {
+                    int RR = IMG_Compare.GetPixel(x, y).R;
+                    int GG = IMG_Compare.GetPixel(x, y).G;
+                    int BB = IMG_Compare.GetPixel(x, y).B;
+
+                    int GRY = (int)(0.3 * RR + 0.58 * GG + 0.12 * BB);
+                    IMG_Compare.SetPixel(x, y, Color.FromArgb(GRY, GRY, GRY));
+                }
+
+            //Generate Histogram
+            int[] Hisot = new int[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                Hisot[i] = 0;
+            }
+            for (int x = 0; x < IMG_Compare.Width; x++)
+            {
+                for (int y = 0; y < IMG_Compare.Height; y++)
+                {
+                    Hisot[IMG_Compare.GetPixel(x, y).R]++;
+                }
+            }
+
+            int Peak1_Level = 64;
+            int Peak1_Val = 0;
+            for (int i = 64; i < 128; i++)
+                if (Hisot[i] > Peak1_Val)
+                {
+                    Peak1_Level = i;
+                    Peak1_Val = Hisot[i];
+                }
+
+            int Peak2_Level = 128;
+            int Peak2_Val = 0;
+            for (int i = 128; i < 192; i++)
+                if (Hisot[i] > Peak2_Val)
+                {
+                    Peak2_Level = i;
+                    Peak2_Val = Hisot[i];
+                }
+
+            int Th_Level = (Peak1_Level + Peak2_Level) / 2; //Average
+
+
+            Letter_H_Proj = new int[IMG_Compare.Height];
+            Letter_V_Proj = new int[IMG_Compare.Width];
+
+            for (int y = 0; y < IMG_Compare.Height; y++)
+                Letter_H_Proj[y] = 0;//buraya bak
+
+            for (int x = 0; x < IMG_Compare.Width; x++)
+                Letter_V_Proj[x] = 0;
+
+            for (int y = 0; y < IMG_Compare.Height; y++)
+                for (int x = 0; x < IMG_Compare.Width; x++)
+                    if (IMG_Compare.GetPixel(x, y).R < Th_Level)
+                    {
+                        Letter_H_Proj[y]++;
+                    }
+            for (int x = 0; x < IMG_Compare.Width; x++)
+                for (int y = 0; y < IMG_Compare.Height; y++)
+                    if (IMG_Compare.GetPixel(x, y).R < Th_Level)
+                    {
+                        Letter_V_Proj[x]++;
+                    }
+            //Shiftting H
+            int Pivot;
+            for (Pivot = 0; Pivot < IMG_Compare.Height; Pivot++)
+                if (Letter_H_Proj[Pivot] > 0) break;
+
+
+            for (int y = 0; y < IMG_Compare.Height - Pivot; y++)
+            {
+                Letter_H_Proj[y] = Letter_H_Proj[y + Pivot];
+                Letter_H_Proj[y + Pivot] = 0;
+            }
+
+            //Shiftting Vertical
+            for (Pivot = 0; Pivot < IMG_Compare.Width; Pivot++)
+                if (Letter_V_Proj[Pivot] > 0) break;
+
+            for (int x = 0; x < IMG_Compare.Width - Pivot; x++)
+            {
+                Letter_V_Proj[x] = Letter_V_Proj[x + Pivot];
+                Letter_V_Proj[x + Pivot] = 0;
+            }
+            //Find Differences
+            int V_Diff = 0;
+            int H_Diff = 0;
+            for (int x = 0; x < IMG_Compare.Width; x++)
+                V_Diff += Math.Abs(Letter_F_V_Proj[x] - Letter_V_Proj[x]);
+
+            for (int y = 0; y < IMG_Compare.Height; y++)
+                H_Diff += Math.Abs(Letter_F_H_Proj[y] - Letter_H_Proj[y]);
+            textBox1.Text = V_Diff.ToString();
+            textBox2.Text = H_Diff.ToString();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String F_Name;
+            openFileDialog1.ShowDialog();
+            F_Name = openFileDialog1.FileName;
+            IMG_Compare = new Bitmap(F_Name);
+            pictureBox2.Image = IMG_Compare;
+        }
+    }
+
+}
+    
+
